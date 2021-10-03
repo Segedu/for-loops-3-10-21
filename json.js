33428d3232a012fc4db7dd57a2be93c55f9317b5
segedu <seggedu20@gmail.com>
2021-10-03 21:55:19 +0300

    finished exercise

---
+++ json.js
@@ -0,0 +1,64 @@
+//!1
+// const person = `{
+// "name":"segedu",
+// "age":24,
+// "city":"jerusalem"
+// }`;
+// console.log(person);
+// console.log(JSON.parse(person));
+
+//!2
+// const car = `{
+//     "model":"mazda",
+//     "year":2021,
+//     "price":120000,
+//     "images":["https://cdn.pixabay.com/photo/2019/07/07/14/03/fiat-4322521__340.jpg","https://cdn.pixabay.com/photo/2015/09/02/12/25/bmw-918408__340.jpg"]
+// }`;
+// console.log(car);
+// console.log(JSON.parse(car));
+
+//!3
+// const book = `{
+//     "name":"Shadow Of The Wind",
+//     "year":2010,
+//     "writer":{"name":"Carlos Luis Zafon", "year of birth":1932}
+// }`;
+// console.log(book);
+// console.log(JSON.parse(book));
+
+//!4
+// const article = {
+//   title: "jjjj",
+//   price: 1230,
+//   discount: 10,
+// };
+// console.log(article);
+// console.log(JSON.stringify(article));
+
+//!5
+// const product = {
+//   name: "aaa",
+//   price: 230,
+//   discount: 30,
+// };
+// console.log(product);
+// console.log(JSON.stringify(product));
+
+//!6
+// const store = `{
+// "products": [
+//     {"name":"lklkl","price":300,"description":"dsdsdsds","img":"https://cdn.pixabay.com/photo/2016/11/22/20/10/dog-1850465__340.jpg"},
+//     {"name":"oioio","price":200,"description":"yuuyuyu","img":"https://cdn.pixabay.com/photo/2016/11/22/20/10/dog-1850465__340.jpg"}
+// ],
+// "categories": ["fiction","drama","romance","docomentary"]
+// }`;
+// const divElement = document.getElementById("cont");
+// let converted = JSON.parse(store);
+
+// converted.products.forEach((element) => {
+//   divElement.innerHTML = `
+//   <p>${element.name}</p>
+//   <h1>${element.price}</h1>
+//   <p>${element.description}</p>
+//   <img src="${element.img}">`;
+// });
